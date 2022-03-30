95527c268c82f0a71307e28bed9d8906bf7aa6db
Yusuf İşleyen <me@yusufisleyen.com>
2022-03-31 00:03:46 +0300

    Dollar rate code added

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type ExchangeRate struct {
+	Usd struct {
+		Code        string  `json:"code"`
+		Rate 		float64 `json:"inverseRate"`
+	} `json:"usd"`
+}
 
 func main() {
-	fmt.Println("get exchange rate")
+	r, err := http.Get("http://www.floatrates.com/daily/try.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer r.Body.Close()
+
+	// status control check
+	if r.StatusCode != http.StatusOK {
+		fmt.Println("Status not successful")
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	var exchangeRate ExchangeRate
+	json.Unmarshal(body, &exchangeRate)
+
+	if err != nil {
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	fmt.Println(exchangeRate)
 }
